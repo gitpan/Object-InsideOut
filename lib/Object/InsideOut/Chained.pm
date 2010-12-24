2c9fbd8ed1d62499f9b7073373bd0284ee232a6f
Jerry D. Hedden <jdhedden@cpan.org>
2010-12-24 18:04:24 +0000

    Import of JDHEDDEN/Object-InsideOut-3.78 from CPAN.
    gitpan-cpan-distribution: Object-InsideOut
    gitpan-cpan-version:      3.78
    gitpan-cpan-path:         JDHEDDEN/Object-InsideOut-3.78.tar.gz
    gitpan-cpan-author:       JDHEDDEN
    gitpan-cpan-maturity:     released

--- lib/Object/InsideOut/Chained.pm
+++ lib/Object/InsideOut/Chained.pm
@@ -181,7 +181,7 @@ sub create_CHAINED :Sub(Private)
 
 
 # Ensure correct versioning
-($Object::InsideOut::VERSION == 3.77)
+($Object::InsideOut::VERSION == 3.78)
     or die("Version mismatch\n");
 
 # EOF
